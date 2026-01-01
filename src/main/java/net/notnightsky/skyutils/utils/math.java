75c359336f9d9b14814038178f6a964bf2c6f4b9
NotNightSky <majumdernupur888@gmail.com>
2026-01-01 16:53:17 +0530

    Fixed interpolationMode not working (translation still needed)

--- src/main/java/net/notnightsky/skyutils/utils/math.java
+++ src/main/java/net/notnightsky/skyutils/utils/math.java
@@ -5,19 +5,44 @@ import net.notnightsky.skyutils.modules.zoom.InterpolationMode;
 public final class math {
 
     public static double interpolate(double start, double end, double speed, double delta, InterpolationMode mode) {
-        double t = 1.0 - Math.exp(-speed * delta);
-        t = Math.clamp(t, 0.0, 1.0);
-        double result;
+        double t;
 
-        return switch (mode) {
-            case LINEAR -> lerp(start, end, t);
-            case LOGARITHMIC -> lerpLog(start, end, t);
-            case EASEINOUTSINE -> lerpLog(start, end, easeInOutSine(t));
-            case EASEOUTCUBIC -> lerpLog(start, end, easeOutCubic(t));
-            case EASEOUTQUAD -> lerpLog(start, end, easeOutQuad(t));
-            case EASEOUTEXPO -> lerpLog(start, end, easeOutExpo(t));
-        };
+        switch (mode) {
+            case LINEAR -> {
+                t = speed * delta;
+                t = Math.clamp(t, 0.0, 1.0);
+                return lerp(start, end, t);
+            }
+
+            case LOGARITHMIC -> {
+                t = 1.0 - Math.exp(-speed * delta);
+                return lerpLog(start, end, t);
+            }
+
+            case EASEINOUTSINE -> {
+                t = Math.clamp(speed * delta, 0.0, 1.0);
+                return lerp(start, end, easeInOutSine(t));
+            }
+
+            case EASEOUTCUBIC -> {
+                t = Math.clamp(speed * delta, 0.0, 1.0);
+                return lerp(start, end, easeOutCubic(t));
+            }
+
+            case EASEOUTQUAD -> {
+                t = Math.clamp(speed * delta, 0.0, 1.0);
+                return lerp(start, end, easeOutQuad(t));
+            }
+
+            case EASEOUTEXPO -> {
+                t = Math.clamp(speed * delta, 0.0, 1.0);
+                return lerp(start, end, easeOutExpo(t));
+            }
+        }
+
+        return end;
     }
+
 
     public static double lerp(double start, double end, double t) {
         return start + (end - start) * t;
